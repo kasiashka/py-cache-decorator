7291fefdd8db76050e042ffb508b2604fa368c66
kasiashka <kasiashka@gmail.com>
2025-03-16 14:01:37 +0200

    Realised decorator "cache"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash_dict = {}
+
+    @wraps(func)
+    def wrapper(*args) -> Callable:
+        if args in cash_dict:
+            print("Getting from cache")
+            return cash_dict[args]
+        else:
+            print("Calculating new result")
+            result = func(*args)
+            cash_dict[args] = result
+            return result
+    return wrapper
